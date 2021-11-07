3acbc608f9872e2ced0b6aeb564929c0ceb929d1
alpunbabun <alfonz.delatorre@ust-legazpi.edu.ph>
2021-11-08 00:53:06 +0800

    Added CutomerRegistrationDto and Maincontroller

---
+++ src/main/java/com/example/Springboot/Web/Maincontroller.java
@@ -0,0 +1,17 @@
+package com.example.Springboot.Web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Maincontroller {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}

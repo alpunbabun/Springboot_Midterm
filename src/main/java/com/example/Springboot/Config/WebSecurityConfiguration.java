e22c5a835861a7e722506098c77c8f3dba706865
alpunbabun <alfonz.delatorre@ust-legazpi.edu.ph>
2021-11-08 00:24:24 +0800

    Added Annotations for WebSecurityConfig

--- src/main/java/com/example/Springboot/Config/WebSecurityConfiguration.java
+++ src/main/java/com/example/Springboot/Config/WebSecurityConfiguration.java
@@ -1,5 +1,11 @@
 package com.example.Springboot.Config;
 
-public class WebSecurityConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 }
